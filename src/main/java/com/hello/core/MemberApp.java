65968462f81d8601603a53688329b40e5b18ba35
myeongkook <myeongkuk@hotmail.com>
2021-09-02 18:47:59 +0900

    "09-02"

---
+++ src/main/java/com/hello/core/MemberApp.java
@@ -0,0 +1,18 @@
+package com.hello.core;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L,"memberA", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(member.getId());
+        System.out.println("NewMember.getName() = " + member.getName());
+        System.out.println("findMember.getName() = " + findMember.getName());
+    }
+}
---
+++ src/main/java/com/hello/core/member/Grade.java
@@ -0,0 +1,5 @@
+package com.hello.core.member;
+
+public enum Grade {
+    BASIC,VIP
+}
