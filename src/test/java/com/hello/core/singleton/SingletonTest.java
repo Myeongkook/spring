bd5b261ff3dfcaaf9d8df6d642d21ced14d09866
Myeongkook Park <myeongkuk@hotmail.com>
2021-09-08 23:03:39 +0900

    Singleton Pattern의 문제점, stateless로 개발하기

--- src/test/java/com/hello/core/singleton/SingletonTest.java
+++ src/test/java/com/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import com.hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -32,5 +33,19 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         singletonService1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService",MemberService.class);
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
+
     }
 }
