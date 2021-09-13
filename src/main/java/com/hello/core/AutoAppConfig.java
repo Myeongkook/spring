0a76d5ffaeef5667ce006d73a9c4f9d612c6b5cf
myeongkook <myeongkuk@hotmail.com>
2021-09-13 12:32:11 +0900

    9/13 Bean 충돌 수정

--- src/main/java/com/hello/core/AutoAppConfig.java
+++ src/main/java/com/hello/core/AutoAppConfig.java
@@ -13,8 +13,5 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-    @Bean(name="memoryMemberRepository")
-    MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
+
 }
