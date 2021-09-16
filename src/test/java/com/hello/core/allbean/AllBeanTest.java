0a83a5c309c75bec371f114b068f5a01a69148b4
myeongkook <myeongkuk@hotmail.com>
2021-09-16 12:52:17 +0900

    9/16 Map,List를 사용한 동적서비스 구현

---
+++ src/test/java/com/hello/core/allbean/AllBeanTest.java
@@ -0,0 +1,46 @@
+package com.hello.core.allbean;
+
+import com.hello.core.AutoAppConfig;
+import com.hello.core.discount.DiscountPolicy;
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+        Assertions.assertThat(discountService).isInstanceOf(DiscountService.class);
+        Assertions.assertThat(discountPrice).isEqualTo(1000);
+        int rateDiscountPolicy = discountService.discount(member, 20000, "rateDiscountPolicy");
+        Assertions.assertThat(rateDiscountPolicy).isEqualTo(2000);
+    }
+
+    static class DiscountService{
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String DiscountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(DiscountCode);
+            return discountPolicy.discount(member,price);
+        }
+    }
+}
